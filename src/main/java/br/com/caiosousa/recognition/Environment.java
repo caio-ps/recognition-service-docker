c75fa94e6e51b6cfcbf731852c06937f2562983b
Caio Pereira <caio.psw@gmail.com>
2018-07-24 22:09:05 -0300

    ComponentScan fix

--- src/main/java/br/com/caiosousa/recognition/Environment.java
+++ src/main/java/br/com/caiosousa/recognition/Environment.java
@@ -2,7 +2,7 @@ package br.com.caiosousa.recognition;
 
 public enum Environment {
 	
-	S3_BUCKET_NAME;
+	S3_BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY;
 	
 	public String value() {
 		return System.getenv(this.name());
--- src/main/java/br/com/caiosousa/startup/Application.java
+++ src/main/java/br/com/caiosousa/startup/Application.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 import br.com.caiosousa.recognition.Environment;
 
 @SpringBootApplication
+@ComponentScan("br.com.caiosousa.recognition")
 public class Application {
 
 	public static void main(String[] args) {
